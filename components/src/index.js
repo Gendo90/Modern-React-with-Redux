61d8f958fccf517e7e6d20e50b456fc09c5ff0d4
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-05 12:30:12 -0800

    feat: Add basic nested components, props to components

--- components/src/index.js
+++ components/src/index.js
@@ -1,26 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.image()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00 PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
-                </div>
-            </div>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 6:00 PM"
+                    commentText="Nice blog post!"
+                    imgSrc={faker.image.image()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Yesterday at 4:25 PM"
+                    commentText="Cool story, bro!"
+                    imgSrc={faker.image.image()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 9:00 AM"
+                    commentText="This is really helpful."
+                    imgSrc={faker.image.image()} />
+            </ApprovalCard>
         </div>)
 };
 
