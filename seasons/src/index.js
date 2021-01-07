b2f1b20ee012778c52f8a233b311a0ecc4778b17
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-07 14:53:13 -0800

    feat: Update app to get and show user's latitude (for season determination)

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    return <div>Hi there!</div>
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { lat: null }
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({ lat: position.coords.latitude}),
+            (err) => console.log(err)
+        )
+    }
+
+    render() {
+        return <div>Latitude: { this.state.lat }</div>
+    }
 }
 
 
