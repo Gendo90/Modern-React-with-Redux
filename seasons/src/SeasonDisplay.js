ecf555f2240cbf9ada8c30d345695ec495feb0dc
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-11 11:35:17 -0800

    feat: Add basic SeasonDisplay logic to show different results for winter/summer depending on location and month

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,36 @@
 import React from "react";
 
+const seasonsConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Brrr, it's chilly!",
+        iconName: "snowflake"
+    }
+}
+
+function getSeason(lat, month) {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? "summer" : "winter";
+    }
+    else {
+        return lat > 0 ? "winter" : "summer";
+    }
+}
+
 const SeasonDisplay = (props) => {
-    console.log(props.lat)
-    return <div>Season Display</div>
+    const season = getSeason(props.lat, new Date().getMonth())
+    const {text, iconName} = seasonsConfig[season];
+
+    return (
+        <div>
+            <i className={`icon ${iconName}`} />
+            <h1>{text}</h1>
+            <i className={`icon ${iconName}`} />
+        </div>
+    )
 }
 
 export default SeasonDisplay;
