867f1aa8dc9d575bbb759e435a5418fda7102de1
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-11 10:45:42 -0800

    refactor: Use alternate state assignment in App class component, move async data request to componentDidMount function, and now show the SeasonDisplay component upon successfully loading

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay"
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
+    // state refactor here possible due to Babel library
+    state = { lat: null, errorMessage: '' }
 
-        this.state = { lat: null, errorMessage: '' }
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => this.setState({ lat: position.coords.latitude}),
             (err) => this.setState({ errorMessage: err.message })
@@ -16,7 +16,7 @@ class App extends React.Component {
     render() {
         if(this.state.errorMessage && !this.state.lat) return <div>Error: { this.state.errorMessage }</div>
 
-        if(!this.state.errorMessage && this.state.lat) return <div>Latitude: { this.state.lat }</div>
+        if(!this.state.errorMessage && this.state.lat) return <SeasonDisplay lat={ this.state.lat } />
 
         return <div>Loading...</div>
     }
