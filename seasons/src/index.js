4eae35d428032ce9ec0432b738c7710153938052
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-13 09:01:02 -0800

    feat: Add example conditional helper method to remove conditionals from render method in App component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -14,12 +14,21 @@ class App extends React.Component {
         )
     }
 
-    render() {
+    //example of a method containing conditional return statements
+    //must be separated from render to enable easier use and addition of
+    //wrapper elements (like a border around any response)
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat) return <div>Error: { this.state.errorMessage }</div>
 
         if(!this.state.errorMessage && this.state.lat) return <SeasonDisplay lat={ this.state.lat } />
 
-        return <Loader />
+        return <Loader message="Please accept the location request" />
+    }
+
+    //example of render containing a helper method wrapped in an element, for
+    //all possible cases of conditional helper method output
+    render() {
+        return <div className="border red">{this.renderContent()}</div>
     }
 }
 
