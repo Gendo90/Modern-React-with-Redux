30a30f9f34fd6bcae46bf99656b0d4e6d44636b6
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-18 13:07:39 -0800

    feat: Enable SearchBar form submit with preventDefault for Pics app

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -6,10 +6,16 @@ class SearchBar extends React.Component {
     //setting the value of the input element of the component (view)
     state = { term: '' }
 
+    onFormSubmit = (event) => {
+        event.preventDefault()
+
+        console.log(this.state.term)
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input
