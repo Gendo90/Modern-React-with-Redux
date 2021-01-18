b9183c282d0cb867021da5e32a9a7b2ad00aa16b
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-18 12:13:27 -0800

    feat: Add input value changes to state for SearchBar component in the "Pics" app - makes component controlled

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    //use MVC ideas to convert component from uncontrolled to
+    //controlled by storing input text in state (model) and then
+    //setting the value of the input element of the component (view)
+    state = { term: '' }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input
+                        type="text"
+                        value={this.state.term}
+                        onChange={ (e) => this.setState({ term: e.target.value })}/>
                     </div>
                 </form>
             </div>
