d0e9057d8cc16438fea3d9561861d1941ca92b59
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-20 13:41:05 -0800

    feat: Add Unsplash Image Search API request capability to Pics app
    Note: Does not use search results yet

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,18 @@
+import axios from "axios";
 import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    onSearchBarSubmit(term) {
-        console.log(term)
+    async onSearchBarSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos',
+        {
+            params: { query: term },
+            headers: {
+                Authorization: "Client-ID WG7F5urzdwVmHq4SLJUKqoJY7cBH6hHE7vqT3Ol9Nvc"
+            }
+        })
+
+        console.log(response.data.results)
     }
 
     render() {
