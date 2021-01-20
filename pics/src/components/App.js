ddd1fdd9e4b52163e5e90d9adc5b4dad770e86e6
Patrick Gendotti <patrick.gendotti@gmail.com>
2021-01-20 14:11:16 -0800

    feat: Store image results from Unsplash API in app state, show number of image page results for Pics app

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,7 +3,11 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    async onSearchBarSubmit(term) {
+    state = { images: [] }
+
+    //needed to use an arrow function to get correct scope - otherwise scope is
+    //props object for the SearchBar component where this function is called
+    onSearchBarSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos',
         {
             params: { query: term },
@@ -12,13 +16,14 @@ class App extends React.Component {
             }
         })
 
-        console.log(response.data.results)
+        this.setState({ images: response.data.results })
     }
 
     render() {
         return (
             <div className="ui container" style={{margin: "10px"}}>
                 <SearchBar onSubmit={this.onSearchBarSubmit} />
+                Page Results: {this.state.images.length}
             </div>
         );
     }
